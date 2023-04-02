221b33e55d3efb66859411a2acb4963d83a23a77
KanoKiyoshi <129677362+KanoKiyoshi@users.noreply.github.com>
2023-04-02 18:57:49 +0300

    Add files via upload

---
+++ people.py.py
@@ -0,0 +1,7 @@
+#створити клас який описує людину
+class people:
+    def __init__(self, name, date_of_birth, gender):
+        self.name = name
+        self.date_of_birth = date_of_birth
+        self.gender = gender
+
